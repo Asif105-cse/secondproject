e68550955f9d393fa35635cf90801e1a46791072
asif <jgcasif11037@gmail.comgit config --global user.emailjgcasif11037@gmail.com>
2025-11-21 20:56:25 +0600

    this is my second commit

--- second.java
+++ second.java
@@ -2,6 +2,9 @@ public class second {
     public static void main(String[] args) {
         System.out.println("my second project");
              System.out.println("github");
+                          System.out.println("ashrai engineering college");
+             System.out.println("Rajshahi ");
+
     }
     
 }
